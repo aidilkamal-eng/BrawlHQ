5263c05c1e622232ddcf61c79a2ac412a2b74cb7
Aidil Kamal <aidilk.b20@mhs.unsyiah.ac.id>
2026-05-07 15:16:01 +0700

    reset deploy 3?

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4124224805" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "939179237" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
